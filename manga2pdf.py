65b4b414e6703e88dc3e3c8f86d6bc008dd7be4c
Gabriel Seixas <salesseixasgabriel@gmail.com>
2018-01-14 15:12:43 -0200

    Start implementing async

--- manga2pdf.py
+++ manga2pdf.py
@@ -1,9 +1,14 @@
-import sys
 from os import listdir, path, makedirs, remove
 from os.path import isfile, join
-from PIL import Image, ImageChops
+import sys
 import glob
+import threading
+import asyncio
+import time
+import aiofiles
 import img2pdf
+from PIL import Image, ImageChops
+
 
 def remove_jpg_images(target_dir, extension):
     filelist = [ f for f in listdir(target_dir) if f.endswith(".{}".format(extension)) ]
@@ -47,23 +52,53 @@ def remove_bottom_border(pil_image):
     return pil_image.crop((0, 0, width, height - 50))
 
 
-def convert_images(image_list):
+def convert_single_image(image):
+    im = Image.open(image).convert('RGB')
+    if '_01_' not in image:
+        im = trim(remove_bottom_border(im))
+
+    im.save(image.replace('webp', 'jpg'), 'jpeg')
+
+
+async def convert_single_image(image):
+    im = Image.open(image).convert('RGB')
+    if '_01_' not in image:
+        await asyncio.sleep(0)
+        im = trim(remove_bottom_border(im))
+    
+    await asyncio.sleep(0)
+    im.save(image.replace('webp', 'jpg'), 'jpeg')
+
+
+async def convert_image_list(image_list):
     for image in image_list:
         if 'webp' in image:
-            im = Image.open(image).convert('RGB')
-            if '_01_' not in image:
-                im = trim(remove_bottom_border(im))
-
-            im.save(image.replace('webp', 'jpg'), 'jpeg')
+            await asynctio.gather(
+                convert_single_image(image)
+            )
 
 
-def convert_to_pdf(image_list, output_name, extension):
+def convert_to_pdf(image_list, output_name):
     pdf_bytes = img2pdf.convert(image_list)
-    with open(output_name, 'wb') as file:
-        file.write(pdf_bytes)
+    with open(output_name, 'wb') as f:
+        f.write(pdf_bytes)
+        f.flush()
 
 
-if __name__ == '__main__':
+def run_async(task):
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(task)
+    finally:
+        loop.close()
+
+
+def show_list_formated(list):
+    for l in list:
+        print(l)
+
+
+def main():
     folder = sys.argv[1]
     output_name = sys.argv[2]
 
@@ -73,17 +108,24 @@ if __name__ == '__main__':
     image_list = find_images_path(folder, '.webp')
     
     print('====Files found====') 
-    print(image_list)
+    show_list_formated(image_list)
 
     print('====Converting and cropping images====')
-    convert_images(image_list)
+    run_async(convert_image_list(image_list))
     converted_images = find_images_path(folder, '.jpg')
-    print(converted_images)
+    show_list_formated(converted_images)
 
     print('====Converting to PDF====')
     jpg_list = find_images_path(folder, '.jpg')
     output_file = '{}/{}'.format(folder, output_name)
-    convert_to_pdf(jpg_list, output_file, '.jpg')
+    convert_to_pdf(jpg_list, output_file)
 
     print('====Removing JPG files====')
     remove_jpg_images(folder, 'jpg')
+
+
+if __name__ == '__main__':
+    start = time.time()
+    main()    
+    end = time.time()
+    print("Total time: {}".format(end - start))
